11303ea9f814da7cb9be68690c02a587c973f8ee
Vladimir Romanov <vladromweb@gmail.com>
2016-08-01 14:25:45 -0700

    Added weather API and URL to begin making JSON calls
    Added weather API and URL to begin making JSON calls

---
+++ MyForecastApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyForecastApp
+//
+//  Created by Volodymyr Romanov on 8/1/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "13999b59d3fbf182030d9d07a17d19f3a1d"
