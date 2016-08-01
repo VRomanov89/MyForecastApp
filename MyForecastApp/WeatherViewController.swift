f36aa688f5b03cdd8a5f42d0df8548587c3d0dcb
Vladimir Romanov <vladromweb@gmail.com>
2016-08-01 13:30:43 -0700

    UI to ViewController links
    UI to ViewController links

--- MyForecastApp/WeatherViewController.swift
+++ MyForecastApp/WeatherViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var weateherImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
